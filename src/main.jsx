0c2cb25031c530111adf19328f0ca19d7e6f5bbb
TaiefSiddique <taiefsiddik2@gmail.com>
2023-09-30 13:18:04 +0600

    login and signup page created and navigation added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
-
+import Home from './Home/Home.jsx';
+import Login from './Login/Login.jsx';
+import Signup from './Signup/Signup.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    children:[
+      {
+      path: "/login",
+      element: <Login/>,      
+      },
+      {
+        path: "/signup",
+        element: <Signup/>,
+      }
+  ]
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
