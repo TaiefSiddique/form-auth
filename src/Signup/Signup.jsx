098cc81b682fb421366f4a6b18dfbd7287f273d8
TaiefSiddique <taiefsiddik2@gmail.com>
2023-09-30 19:40:27 +0600

    Signup Page Done

--- src/Signup/Signup.jsx
+++ src/Signup/Signup.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const Signup = () => {
+    const handleSignup= e =>{
+        e.preventDefault();
+        console.log(e.target.email.value);
+        console.log(e.target.password.value);
+    }
     return (
         <div>
-            <h1> This is signup</h1>
+            <h1 className='text-4xl font-semibold mx-auto w-max m-8'>New to this website!!</h1>
+            <form onSubmit={handleSignup} className=''>
+               <div className='flex flex-col w-3/4 mx-auto h-screen m-8  '>
+               <input type="email" name="email" className='p-4 mb-4 rounded-sm'/>
+                <input type="password" name="password"  className='p-4 mb-4 rounded-sm'/>
+                <button type="submit" className="btn btn-secondary rounded-sm">SignUp</button>
+               </div>
+            </form>
         </div>
     );
 };
